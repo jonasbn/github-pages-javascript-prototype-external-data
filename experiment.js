9228ecc6ea403262f1aca909614ab148592df2bd
jonasbn <jonasbn@gmail.com>
2019-11-03 15:14:06 +0100

    Added timeout so change can be observed

--- experiment.js
+++ experiment.js
@@ -1,14 +1,18 @@
 function reqListener () {
     var obj = JSON.parse(this.responseText);
     console.log(obj);
-    const username = document.querySelector('div.username')
-    username.textContent = obj.data.first_name + " " + obj.data.last_name;
 
-    const bio = document.querySelector('div.bio')
-    bio.textContent = obj.data.email;
+    setTimeout(function(){
 
-    const avatar = document.querySelector('img.avatar');
-    avatar.src = obj.data.avatar;
+        const username = document.querySelector('div.username')
+        username.textContent = obj.data.first_name + " " + obj.data.last_name;
+
+        const bio = document.querySelector('div.bio')
+        bio.textContent = obj.data.email;
+
+        const avatar = document.querySelector('img.avatar');
+        avatar.src = obj.data.avatar;
+    }, 2000);
 }
 
 var oReq = new XMLHttpRequest();
